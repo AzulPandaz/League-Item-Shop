56b579c01e5599405dec7745eff1e7ab4df27be0
rogeliovalencia281@gmail.com <rogeliovalencia281@gmail.com>
2022-08-26 12:33:44 -0500

    changed router and routes

---
+++ express/app/routes/api/supportItemsRoutes.js
@@ -0,0 +1,19 @@
+// const express= require('express')
+// const router = express.Router()
+
+// const {longboardDao: dao} = require('../../daos/dao')
+// //const { route } = require('./filmRoutes')
+// // const router = require('./filmRoutes')
+
+// router.get('/', (req,res)=>{
+//     dao.findAll(res, dao.table)
+// })
+
+// router.get('/:id', (req,res)=>{
+//     dao.findById(res, dao.table, req.params.id)
+// })
+
+
+
+
+// module.exports = router
--- express/app/routes/router.js
+++ express/app/routes/router.js
@@ -6,20 +6,21 @@ const fetch = (...args)=>import('node-fetch').then(({default:fetch})=>fetch(...a
 // api
 router.get('/', (req,res)=>{
     res.json({
-        "ALL Skateboards":`http://localhost:${PORT}/api/items`,
-        'ALL Longboards': `http://localhost:${PORT}/api/items/longboard`,
-        "All Street Boards":`http://localhost:${PORT}/api/items/streetboard`,
-        "All Penny Boards":`http://localhost:${PORT}/api/items/pennyboard`,
-        "All Cruiser Boards":`http://localhost:${PORT}/api/items/cruiserboard`
+        "ALL Items":`http://localhost:${PORT}/api/items`,
+        'ALL Fighter Items': `http://localhost:${PORT}/api/items/fighter`,
+        "All Marksman Items":`http://localhost:${PORT}/api/items/marksman`,
+        "All Tank Items":`http://localhost:${PORT}/api/items/Tank`,
+        "All Assassin Items":`http://localhost:${PORT}/api/items/assassin` ,
+        "All Support Items":`http://localhost:${PORT}/api/items/support`
     })
 })
-router.use('/items/longboard', require('./api/longboardRoutes'))
-router.use('/items/cruiserboard', require('./api/cruiserboardRoutes'))
-router.use('/items/pennyboard', require('./api/pennyboardRoutes'))
+router.use('/items/longboard', require('./api/marksmanItemsRoutes'))
+router.use('/items/cruiserboard', require('./api/supportItems'))
+router.use('/items/pennyboard', require('./api/fighterItemsRoutes'))
+router.use('/items/streetboard', require('./api/tankItemsRoutes'))
+router.use('/items', require('./api/assassinItemsRoutes'))
+router.use('/items', require('./api/supportItemsRoutes'))
 
-router.use('/items/streetboard', require('./api/streetboardRoutes'))
-
-router.use('/items', require('./api/skateboardRoutes'))
 
 
 
